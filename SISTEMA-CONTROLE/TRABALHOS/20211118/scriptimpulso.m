48fb3c7950acdf48093939009840df303104741c
Iago Costa <univercomput@gmail.com>
2021-11-19 00:34:39 -0300

    ===

--- SISTEMA-CONTROLE/TRABALHOS/20211118/scriptimpulso.m
+++ SISTEMA-CONTROLE/TRABALHOS/20211118/scriptimpulso.m
@@ -1,8 +1,8 @@
 pkg load control;
-s = tf('s') #modelo para função de transferência
-f = 5/(s+2) #função de transferência
-#num = [1]
-#dem = [1 0.2 1]
+#s = tf('s') #modelo para função de transferência
+#f = 5/(s+2) #função de transferência
+num = [5]
+dem = [1 1 2]
 f = tf(num, dem)
 impulse(f); #aplicando a resposta ao impulso na função de transferência
 grid
